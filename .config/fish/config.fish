48179ed2578fa7b2c50e14f5eb708ae742209c94
James Robinson <james.robinson@digital.cabinet-office.gov.uk>
2025-05-12 18:32:05 +0100

    Various fish tweaks

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,6 +8,7 @@ if status is-interactive
     abbr --add ga 'git add'
     abbr --add gap 'git add --patch'
     abbr --add gc 'git commit'
+    abbr --add gca 'git commit --amend --no-edit'
     abbr --add gco 'git checkout'
     abbr --add gd 'git diff'
     abbr --add gdc 'git diff --cached'
@@ -15,8 +16,23 @@ if status is-interactive
     abbr --add gp 'git push'
     abbr --add gpf 'git push --force-with-lease'
     abbr --add gpl 'git pull'
+    abbr --add gr 'git rebase'
+    abbr --add grc 'git rebase --continue'
     abbr --add gri 'git rebase -i'
     abbr --add gs 'git status'
 
+    set -x EDITOR nvim
     starship init fish | source
+    rbenv init - fish | source
+    starship init fish | source
+    set -x PATH $PATH:~/.dotfiles/bin
+
+    if type gds > /dev/null
+        gds shell-completion fish | source
+        /usr/bin/ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+        set -x NOTIFY_CREDENTIALS ~/notify/notifications-credentials
+        set -x PASSWORD_STORE_DIR $NOTIFY_CREDENTIALS
+        set -x GPG_TTY (tty)
+        set -x PATH $PATH:(go env GOPATH)/bin
+    end
 end
