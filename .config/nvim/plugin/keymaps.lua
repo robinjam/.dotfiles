2302f18675c65b37c10778b6ea8405e921079b8e
James Robinson <james@robinjam.net>
2025-11-29 15:38:41 +0000

    nvim: Add <leader>w and <leader>q keymaps

--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -1,4 +1,7 @@
 local set = vim.keymap.set
 
+set("n", "<leader>w", "<cmd>w<CR>")
+set("n", "<leader>q", "<cmd>q<CR>")
+
 -- Clear highlights on search when pressing <Esc> in normal mode
 set("n", "<Esc>", "<cmd>nohlsearch<CR>")
