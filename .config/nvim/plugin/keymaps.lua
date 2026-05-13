e53925f30fb65ec0923df548f46a0f993f85bd78
James Robinson <james@robinjam.net>
2026-05-13 21:01:11 +0100

    nvim: Add <leader>td shortcut to toggle diagnostics

--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -5,3 +5,8 @@ set("n", "<leader>q", "<cmd>q<CR>")
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+-- Toggle diagnostics
+set("n", "<leader>td", function()
+  vim.diagnostic.enable(not vim.diagnostic.is_enabled())
+end, { desc = "[T]oggle [d]iagnostics" })
